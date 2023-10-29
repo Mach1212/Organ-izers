58a2ea91a4baab7b850ef79cb5a04f70bb92e4a1
mach1212 <maciej.pruchnik@gmail.com>
2023-10-28 23:17:34 -0400

    [Feat] Pass className props to map

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -7,8 +7,9 @@ const Map = dynamic(() => import("@/components/map"), {
 
 export default function App() {
   return (
-    <main className="h-screen w-screen">
-      <Map />
+    <main className="h-screen w-screen flex">
+      <Map className="h-full w-full" />
+      <Sidebar />
     </main>
   );
 }
--- frontend/components/map.js
+++ frontend/components/map.js
@@ -3,13 +3,14 @@
 import "leaflet/dist/leaflet.css";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
-const Map = () => {
+const Map = (props) => {
     return (
         <MapContainer
             center={[35.909740, -79.047291]}
             zoom={13}
             scrollWheelZoom={false}
             style={{ height: "100%", width: "100%" }}
+            className={props.className}
         >
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
