6822224c66dc882b0805b562a1bbad8c7095f57a
mach1212 <maciej.pruchnik@gmail.com>
2023-10-29 05:23:27 -0400

    [Feat] Implemented sidebar

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -2,34 +2,78 @@
 
 import Sidebar from "@/components/sidebar";
 import dynamic from "next/dynamic";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useMapEvents } from "react-leaflet";
+import { Surreal } from "surrealdb.js";
 
 const Map = dynamic(() => import("@/components/map"), {
   ssr: false,
 });
 
+const db = new Surreal();
+const getHospitals = () => db.select("Hospital");
+
 export default function App() {
-  let [showSidebar, setShowSidebar] = useState(true);
-  const visibleSidebar = () => {
-    console.log("show");
+  let [hospitals, setHospitals] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      await setupDB();
+      const hospitals = await getHospitals();
+      setHospitals(hospitals);
+    };
+    getData();
+  }, []);
+
+  let [showSidebar, setShowSidebar] = useState(false);
+  let [sidebarHospital, setSidebarHospital] = useState({});
+
+  const visibleSidebar = (hospital) => {
+    setSidebarHospital(hospital);
     setShowSidebar(true);
   };
   const hideSidebar = () => {
-    console.log("hide");
     setShowSidebar(false);
   };
 
   return (
     <main className="h-screen w-screen flex">
       <div className="h-full w-full">
-        <Map visibleSidebar={visibleSidebar} />
+        <Map
+          visibleSidebar={visibleSidebar}
+          hospitals={hospitals}
+        />
       </div>
       <Sidebar
-        className="w-80 h-full bg-red-400"
+        className="w-80 h-full bg-neutral-100"
         toggleClasses="-right-full absolute hidden"
         hideSidebar={hideSidebar}
         showSidebar={showSidebar}
+        hospital={sidebarHospital}
       />
     </main>
   );
+}
+
+async function setupDB() {
+  try {
+    // Connect to the database
+
+    const namespace = "namespace-name";
+    const database = "database-name";
+    await db.connect(`ws://localhost:8082/rpc`, {
+      // Set the namespace and database for the connection
+      ns: namespace,
+      db: database,
+
+      // Set the authentication details for the connection
+      auth: {
+        NS: namespace,
+        DB: database,
+        user: "hacknc",
+        pass: "a_totally_secure_password_is_really_long",
+      },
+    });
+  } catch (e) {
+    console.error("ERROR", e);
+  }
 }
--- frontend/components/map.js
+++ frontend/components/map.js
@@ -5,7 +5,7 @@ import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet-defaulticon-compatibility";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 
-function Map({ visibleSidebar }) {
+function Map({ visibleSidebar, hospitals }) {
     return (
         <MapContainer
             center={[35.909740, -79.047291]}
@@ -17,14 +17,17 @@ function Map({ visibleSidebar }) {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker
-                position={[35.909740, -79.047291]}
-                eventHandlers={{ click: visibleSidebar }}
-            >
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
+            {hospitals.map((hospital) => (
+                <Marker
+                    key={hospital.name}
+                    position={[hospital.latitude, hospital.longitude]}
+                    eventHandlers={{ click: () => visibleSidebar(hospital) }}
+                >
+                    <Popup>
+                        {hospital.name}
+                    </Popup>
+                </Marker>
+            ))}
         </MapContainer>
     );
 }
